19b0dcd83be2e0960de716a947ad30d3bd0842b6
ejetli <etah4077@gmail.com>
2018-12-23 22:34:30 +0100

    till to be updated

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <section class="hero has-text-centered is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">PWA News</h1>
+                    <h2 class="subtitle">All the headlines making the wavy!</h2>
+                </div>
+            </div>
+        </section>
+        <nuxt/>
+    </div>
+</template>
+
+<style>
+    html {
+        font-family: &apos;Source Sans Pro&apos;, -apple-system, BlinkMacSystemFont, Roboto, &apos;Helvetica Neue&apos;, Arial, sans-serif;
+    }
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,71 @@
+const webpack = require('webpack')
+
+module.exports = {
+ /** Headers of the page
+  */
+  head: {
+    title: 'pwa-news',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+
+  modules: [
+    // Doc: https://github.com/nuxt-community/axios-module#usage
+    '@nuxtjs/axios',
+    // Doc: https://bootstrap-vue.js.org/docs/
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/dotenv',
+    '@nuxtjs/bulma',
+    '@nuxtjs/pwa'
+  ],
+  /*
+  ** Axios module configuration
+  */
+  axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+  },
+
+
+  /*
+  ** Build configuration
+  */
+  build: {
+    
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+    /* Run ESLint on save
+    */ vendor: ['jquery', 'bootstrap'],
+    plugins: [
+      // set shortcuts as global for bootstrap
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery'
+      })
+    ]
+  }
+},
+   // include bootstrap css
+  css: ['bootstrap/dist/css/bootstrap.css'],
+  // include bootstrap js on startup
+  plugins: ['~plugins/bootstrap.js'],
+
